3017833b3a3e382d7f53669cbfe58946e522f1d7
Arjay Angeles <aqangeles@gmail.com>
2017-06-04 16:39:14 +0800

    Use admin-lte box style.

--- Resources/views/widgets/referrers.blade.php
+++ Resources/views/widgets/referrers.blade.php
@@ -1,8 +1,14 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><i class="fa fa-truck"></i> Top Referrers</h3>
+<div class="box box-danger">
+    <div class="box-header with-border">
+        <h3 class="box-title"><i class="fa fa-truck"></i> Top Referrers</h3>
+
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                <i class="fa fa-minus"></i>
+            </button>
+        </div>
     </div>
-    <div class="panel-body">
+    <div class="box-body">
         <table class="table">
             <thead>
             <tr>
