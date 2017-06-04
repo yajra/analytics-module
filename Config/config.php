494c384db7cb24db4a46c45108339de8f5e4b841
Arjay Angeles <aqangeles@gmail.com>
2017-06-04 12:12:02 +0800

    (wip) Add test route for spatie analytics.

--- Config/config.php
+++ Config/config.php
@@ -2,9 +2,9 @@
 
 return [
 	'name'        => 'Analytics',
-	'description' => 'Analytics module description.',
-	'keywords'    => '$LOWER_NAME$, laravel, module',
-	'author'      => '$AUTHOR_NAME$',
+	'description' => 'Analytics module.',
+	'keywords'    => 'analytics, laravel, module',
+	'author'      => 'Arjay Angeles',
 	'version'     => '1.0.0-dev',
-	'icon'        => 'fa fa-file icon-$LOWER_NAME$',
+	'icon'        => 'fa fa-file icon-analytics',
 ];
--- Http/routes.php
+++ Http/routes.php
@@ -6,17 +6,23 @@
  */
 
 use Modules\Analytics\Http\Controllers\AnalyticsController;
+use Spatie\Analytics\Analytics;
+use Spatie\Analytics\Period;
 
 $router = app('router');
 
 // Front-end routes.
-$router->group(['middleware' => 'web'], function() use ($router) {
-	$router->resource('analytics', AnalyticsController::class);
+$router->group(['middleware' => 'web'], function () use ($router) {
+    $router->resource('analytics', AnalyticsController::class);
 });
 
 // Back-end routes.
-$router->group(['middleware' => 'administrator', 'prefix' => 'administrator'], function() use ($router) {
-	$router->get('analytics', function() {
-	    return view('analytics::backend.index');
-	});
+$router->group([
+    'middleware' => 'administrator',
+    'prefix'     => 'administrator',
+    'as'         => 'administrator.',
+], function () use ($router) {
+    $router->get('analytics', function (Analytics $analytics) {
+        return $analytics->fetchVisitorsAndPageViews(Period::days(7));
+    })->name('analytics.index');
 });
